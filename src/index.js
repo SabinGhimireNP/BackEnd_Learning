63b3a7818cfd58fa568e52a21a1e26744da867aa
SabinGhimireNP <sabinghimire223@gmail.com>
2026-05-25 21:28:43 +0545

    Utils added

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import connectDB from "./db/index.js"
 
 dotenv.config({path: "./env"})
 
-connectDB()
+connectDB().then(()=>{
+    //Error in the database
+    app.on("Error",(error)=>{
+        console.log("Error: ",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT||800, ()=>{
+        console.log(`Server is Running on port: ${process.env.PORT ||8000}`)
+    })
+}).catch((error)=>{
+    //Error during connection with database
+    console.log(" DataBase conncection failed:", error)
+})
